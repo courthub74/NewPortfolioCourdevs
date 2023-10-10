39b5473e57e9832cf03b525328efe8c446b3e906
Courdevelops <courdevelops@gmail.com>
2023-10-10 14:03:21 -0400

    first commit

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+// Portfolio JS
+
+// Query Apps
+
+// App 1
+let app_1 = document.getElementById('app_1');
+// App 2
+let app_2 = document.getElementById('app_2');
+// App 3
+let app_3 = document.getElementById('app_3');
+// App 4
+let app_4 = document.getElementById('app_4');
+
+// add an event listener to each app button
+    // to it make a 'click' event function
+    //  to the 'click' event function toggle the css class 
+        // to slide the description in from the right
